ab3f2e309d5d01552f35e34d7fec08cf15a76495
Kyle <kyle90adam@hotmail.com>
2014-02-06 22:52:00 +0000

    Error code checking added to test class.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import uk.co.kyleharrison.grapejuice.utils.URLReader;
 
 public class Main {
@@ -5,8 +9,33 @@ public class Main {
 	public static void main(String[] args) {
 		URLReader urlReader = new URLReader();
 		urlReader.setUrl("http://www.comicvine.com/api/publishers/?api_key=2736f1620710c52159ba0d0aea337c59bd273816&format=json&sort=name&field_list=name,id&offset=2300");;
-		System.out.println(urlReader.readFromUrl());
-
+		String jsonResponse = urlReader.readFromUrl();
+		System.out.println(jsonResponse);
+		
+		if(jsonResponse!=null){
+			try {
+				JSONObject json = (JSONObject)new JSONParser().parse(jsonResponse);
+				System.out.println(json);
+				
+				System.out.println(json.get("error").toString());
+				System.out.println(json.get("status_code"));
+				
+				if(Integer.parseInt(json.get("status_code").toString())==1){
+					System.out.println(json.get("status_code"));
+					System.out.println(json.get("results"));
+				}
+				
+				if(json.get("error").toString().equalsIgnoreCase("OK")){
+					System.out.println(json.get("error"));
+					System.out.println(json.get("results"));
+				}
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
