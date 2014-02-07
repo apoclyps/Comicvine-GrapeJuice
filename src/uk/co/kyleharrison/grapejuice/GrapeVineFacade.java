58d5c3ac0d37ed71d3c9956f6a750c4181b02405
apoclyps <kyle90adam@hotmail.com>
2014-02-07 14:31:34 +0000

    GrapeVineFacade updated to include search

--- src/uk/co/kyleharrison/grapejuice/GrapeVineFacade.java
+++ src/uk/co/kyleharrison/grapejuice/GrapeVineFacade.java
@@ -1,9 +1,16 @@
 package uk.co.kyleharrison.grapejuice;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import uk.co.kyleharrison.grapejuice.comicvine.ComicVineVolume;
 import uk.co.kyleharrison.grapejuice.utils.URLReader;
 
 public class GrapeVineFacade {
@@ -23,18 +30,36 @@ public class GrapeVineFacade {
 				json = (JSONObject)new JSONParser().parse(jsonResponse);
 				System.out.println(json);
 				
-				System.out.println(json.get("error").toString());
-				System.out.println(json.get("status_code"));
+			    //System.out.println(json.get("error").toString());
+				//System.out.println(json.get("status_code"));
 				
 				if(Integer.parseInt(json.get("status_code").toString())==1){
-					System.out.println(json.get("status_code"));
-					System.out.println(json.get("results"));
+					//System.out.println(json.get("status_code"));
+					//System.out.println(json.get("results"));
 				}
 				
 				if(json.get("error").toString().equalsIgnoreCase("OK")){
-					System.out.println(json.get("error"));
-					System.out.println(json.get("results"));
+					//System.out.println(json.get("error"));
+					//System.out.println(json.get("results"));
 				}
+				
+				JSONArray jsonArray2 = (JSONArray) json.get("results");
+				System.out.println(jsonArray2.toString());
+				
+				for(Object obj : jsonArray2){
+					JSONObject jsoObj = (JSONObject) obj;
+					try{
+					if(jsoObj.containsKey("name")){
+						if(!(jsoObj.get("name").toString().equals("null"))){
+						System.out.println(jsoObj.get("id")+ " : " +jsoObj.get("name"));
+						}
+					//mapToPojo(jsoObj);
+					}}
+					catch(NullPointerException e){
+						//e.printStackTrace();
+					}
+				}
+				System.out.println("Total Results : "+jsonArray2.size());
 				
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
@@ -42,6 +67,24 @@ public class GrapeVineFacade {
 			}
 		}
 		return json.toString();
+	}
+	
+	public static void mapToPojo(JSONObject json){
+		ObjectMapper mapper = new ObjectMapper();
+		
+		try {
+			ComicVineVolume user = mapper.readValue(json.toJSONString(), ComicVineVolume.class);
+			System.out.println(user.getId() + " "+ user.getName());
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
