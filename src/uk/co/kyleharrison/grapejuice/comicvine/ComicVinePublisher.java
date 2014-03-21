7e8dab13a06f19d1b93ed5a3a2223df236eba57f
apoclyps <kyle90adam@hotmail.com>
2014-03-21 20:55:23 +0000

    Comicvine publisher updated with Hashmap and getCassandraMap Method. Currently untested and some additional fields may be required from the comicvine API JSON Response.
    Additional work will be required to extend a base class to allow all
    classes to share common methods to reduce the amount of code required.
    ComicVineBase with ID, Name, URL, etc

--- src/uk/co/kyleharrison/grapejuice/comicvine/ComicVinePublisher.java
+++ src/uk/co/kyleharrison/grapejuice/comicvine/ComicVinePublisher.java
@@ -1,10 +1,61 @@
 package uk.co.kyleharrison.grapejuice.comicvine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ComicVinePublisher {
 	public int id;
 	public String name;
     public String publisher_url;
+    public HashMap<String, String> comicvinePublisher;
     
+	public void setComicvinePublisher(){
+		this.comicvinePublisher.put("id", Integer.toString(this.id));
+		this.comicvinePublisher.put("name", this.name);
+		this.comicvinePublisher.put("publisher_url", this.publisher_url);
+	}
+	
+	//Untested
+	public String generateCassandraMap() {
+		this.comicvinePublisher = new HashMap<String,String>();
+		setComicvinePublisher();
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		
+		//System.out.printf("Size : %s \n",this.comicvineIssue.size());
+		int elements = 0;
+		for(Map.Entry<String,String> entry : this.comicvinePublisher.entrySet()){
+			sb.append("'"+entry.getKey().toString()+"' : '" + entry.getValue().toString()+"'");
+			if(elements < comicvinePublisher.entrySet().size()-1){
+				sb.append(", ");
+			}
+			elements++;
+		}
+		sb.append("}");
+		//System.out.println(sb.toString());
+		return (this.cassandraMap = sb.toString());
+	}
+	
+    
+    public HashMap<String, String> getComicvinePublisher() {
+		return comicvinePublisher;
+	}
+
+	public void setComicvinePublisher(HashMap<String, String> comicvinePublisher) {
+		this.comicvinePublisher = comicvinePublisher;
+	}
+	public String cassandraMap = null;
+    
+	public String getCassandraMap() {
+		return cassandraMap;
+	}
+
+	public void setCassandraMap(String cassandraMap) {
+		this.cassandraMap = cassandraMap;
+	}
+
+	
+	
 	public ComicVinePublisher() {
 		super();
 	}
